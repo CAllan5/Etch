f193517250607fd0a5adce37afd089ee31fcb0c7
callan <chris.allan5@gmail.com>
2022-02-02 09:36:44 -0800

    added comments to JS

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const mainBoard = document.querySelector('#main');
 
 
 // for loop creates the first div grid of 16 divs.
-
+// see makeTiles function for further info.
 for(let i = 0; i < 16; i++){
 
     let RanNumA = Math.ceil(Math.random() * 255);
@@ -42,9 +42,9 @@ inputNumber.addEventListener('keydown', (e)=>{
 
 // function to remove then create a new grid bases
 // on the users spicified grid number.
-
 function makeTiles(inputNum){
 
+// while loop clears previous grid.
 while(mainBoard.firstChild){
     mainBoard.removeChild(mainBoard.firstChild);
 }
@@ -59,13 +59,18 @@ if(inputNum > 100){
     let tileSize = (900 / inputNum);
     
  
-
+// for loop for creating divs 'tiles'.
     for(let i = 0; i < tileNum; i++){
+
+    // variables for random color.
 
     let RanNumA = Math.ceil(Math.random() * 255);
     let RanNumB = Math.ceil(Math.random() * 255);
     let RanNumC = Math.ceil(Math.random() * 255);
 
+    // this section of for loop creats and adds each
+    // new tile to the parent element 'mainBoard'.
+    // with the specified properties.
     const tiles = document.createElement('div');
     tiles.style.width = tileSize + 'px';
     tiles.style.height = tileSize + 'px';
